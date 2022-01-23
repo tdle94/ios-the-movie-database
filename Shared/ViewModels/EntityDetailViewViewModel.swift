4956cc3a77524f5da263596833de5b7168c66969
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-23 17:17:07 -0500

    add Published type

--- Shared/ViewModels/EntityDetailViewViewModel.swift
+++ Shared/ViewModels/EntityDetailViewViewModel.swift
@@ -13,7 +13,14 @@ class EntityDetailViewViewModel: ObservableObject {
     let id: Int
     let navigationTitle: String
     let movieDB = MovieDB()
+    var mediaType: MediaType = .backdrop
+    @Published var mediaSelected: Bool = true
     @Published var entityDetail: DisplayObject = DisplayObject(id: 0, titleWithYear: "", backdropLink: "", posterLink: "", overview: "", tagline: "")
+    
+    enum MediaType: String {
+        case backdrop = "Backdrops"
+        case poster = "Posters"
+    }
 
     init(id: Int, navigationTitle: String) {
         self.id = id
@@ -26,8 +33,21 @@ class EntityDetailViewViewModel: ObservableObject {
             let imageResponse = try await movieDB.images(id: id, language: "").get()
             entityDetail = detailResponse.displayObject
             entityDetail.image = imageResponse
+            entityDetail.displayImageLinks = Array(imageResponse.backdropLinks.prefix(5))
         } catch let error {
             print(error)
+        }
+    }
+
+    func selectMediaType(_ type: MediaType) {
+        mediaSelected.toggle()
+        switch type {
+        case .backdrop:
+            entityDetail.displayImageLinks = Array(entityDetail.image?.backdropLinks.prefix(5) ?? [])
+            mediaType = .backdrop
+        case .poster:
+            entityDetail.displayImageLinks = Array(entityDetail.image?.posterLinks.prefix(5) ?? [])
+            mediaType = .poster
         }
     }
 }
