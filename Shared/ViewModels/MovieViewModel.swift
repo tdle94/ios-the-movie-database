df082c01aab1bde3620d4435882bde11f3658581
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-22 21:08:22 -0500

    use id

--- Shared/ViewModels/MovieViewModel.swift
+++ Shared/ViewModels/MovieViewModel.swift
@@ -23,28 +23,28 @@ class MovieViewModel: ObservableObject {
 class PopularMovieViewModel: MovieViewModel {
     override func fetch() async throws {
         let movieResults = try await movieDB.popular().get().results
-        homeViewObjects = movieResults.compactMap { HomeViewObject(title: $0.originalTitle, posterLink: $0.posterLink) }
+        homeViewObjects = movieResults.compactMap { HomeViewObject(id: $0.id, title: $0.originalTitle, posterLink: $0.posterLink) }
     }
 }
 
 class NowPlayingMovieViewModel: MovieViewModel {
     override func fetch() async throws {
         let movieResults = try await movieDB.nowPlaying().get().results
-        homeViewObjects = movieResults.compactMap { HomeViewObject(title: $0.originalTitle, posterLink: $0.posterLink) }
+        homeViewObjects = movieResults.compactMap { HomeViewObject(id: $0.id, title: $0.originalTitle, posterLink: $0.posterLink) }
     }
 }
 
 class UpcomingMovieViewModel: MovieViewModel {
     override func fetch() async throws {
         let movieResults = try await movieDB.upcoming().get().results
-        homeViewObjects = movieResults.compactMap { HomeViewObject(title: $0.originalTitle, posterLink: $0.posterLink) }
+        homeViewObjects = movieResults.compactMap { HomeViewObject(id: $0.id, title: $0.originalTitle, posterLink: $0.posterLink) }
     }
 }
 
 class TopRatedMovieViewModel: MovieViewModel {
     override func fetch() async throws {
         let movieResults = try await movieDB.topRated().get().results
-        homeViewObjects = movieResults.compactMap { HomeViewObject(title: $0.originalTitle, posterLink: $0.posterLink) }
+        homeViewObjects = movieResults.compactMap { HomeViewObject(id: $0.id, title: $0.originalTitle, posterLink: $0.posterLink) }
     }
 }
 
@@ -59,6 +59,6 @@ class LatestMovieViewModel: MovieViewModel {
             posterPath = images.posters.first?.filePath ?? images.backdrops.first?.filePath
         }
         
-        homeViewObjects = [HomeViewObject(title: latestMovie.originalTitle, posterLink: "https://image.tmdb.org/t/p/original/" + (posterPath ?? ""))]
+        homeViewObjects = [HomeViewObject(id: latestMovie.id,title: latestMovie.originalTitle, posterLink: "https://image.tmdb.org/t/p/original/" + (posterPath ?? ""))]
     }
 }
