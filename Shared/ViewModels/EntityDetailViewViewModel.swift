57b9ab5728199ddd18167f5e97aaf73891e0d518
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-23 14:15:31 -0500

    use common object to display

--- Shared/ViewModels/EntityDetailViewViewModel.swift
+++ Shared/ViewModels/EntityDetailViewViewModel.swift
@@ -6,13 +6,28 @@
 //
 
 import Foundation
+import TMDBAPI
 
+@MainActor
 class EntityDetailViewViewModel: ObservableObject {
     let id: Int
     let navigationTitle: String
+    let movieDB = MovieDB()
+    @Published var entityDetail: DisplayObject = DisplayObject(id: 0, titleWithYear: "", backdropLink: "", posterLink: "", overview: "", tagline: "")
 
     init(id: Int, navigationTitle: String) {
         self.id = id
         self.navigationTitle = navigationTitle
+    }
+    
+    func fetchDetail() async throws {
+        do {
+            let detailResponse = try await movieDB.detail(id: id).get()
+            let imageResponse = try await movieDB.images(id: id, language: "").get()
+            entityDetail = detailResponse.displayObject
+            entityDetail.image = imageResponse
+        } catch let error {
+            print(error)
+        }
     }
 }
