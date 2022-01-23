db3d1fd59dd6e6a4fb46c922ee45ad06e74526d7
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-22 21:08:45 -0500

    provide id parameter

--- Shared/ViewModels/TVShowViewModel.swift
+++ Shared/ViewModels/TVShowViewModel.swift
@@ -24,34 +24,34 @@ class TVShowViewModel: ObservableObject {
 class OnTheAirTVShowViewModel: TVShowViewModel {
     override func fetch() async throws {
         let tvShowResults = try await tvShowDB.onTheAir().get().results
-        homeViewObjects = tvShowResults.compactMap { HomeViewObject(title: $0.originalName, posterLink: $0.posterLink) }
+        homeViewObjects = tvShowResults.compactMap { HomeViewObject(id: $0.id, title: $0.originalName, posterLink: $0.posterLink) }
     }
 }
 
 class PopularTVShowViewModel: TVShowViewModel {
     override func fetch() async throws {
         let tvShowResults = try await tvShowDB.popular().get().results
-        homeViewObjects = tvShowResults.compactMap { HomeViewObject(title: $0.originalName, posterLink: $0.posterLink) }
+        homeViewObjects = tvShowResults.compactMap { HomeViewObject(id: $0.id, title: $0.originalName, posterLink: $0.posterLink) }
     }
 }
 
 class TopRatedTVShowViewModel: TVShowViewModel {
     override func fetch() async throws {
         let tvShowResults = try await tvShowDB.topRated().get().results
-        homeViewObjects = tvShowResults.compactMap { HomeViewObject(title: $0.originalName, posterLink: $0.posterLink) }
+        homeViewObjects = tvShowResults.compactMap { HomeViewObject(id: $0.id, title: $0.originalName, posterLink: $0.posterLink) }
     }
 }
 
 class AiringTodayTVShowViewModel: TVShowViewModel {
     override func fetch() async throws {
         let tvShowResults = try await tvShowDB.airingToday().get().results
-        homeViewObjects = tvShowResults.compactMap { HomeViewObject(title: $0.originalName, posterLink: "https://image.tmdb.org/t/p/w200" + ($0.posterPath ?? "")) }
+        homeViewObjects = tvShowResults.compactMap { HomeViewObject(id: $0.id, title: $0.originalName, posterLink: "https://image.tmdb.org/t/p/w200" + ($0.posterPath ?? "")) }
     }
 }
 
 class LatestTVShowViewModel: TVShowViewModel {
     override func fetch() async throws {
         let latestTVShowResult = try await tvShowDB.latest(language: Locale.currentCountryAndLanguageCode).get()
-        homeViewObjects = [HomeViewObject(title: latestTVShowResult.originalName, posterLink: "https://image.tmdb.org/t/p/w200" + (latestTVShowResult.posterPath ?? ""))]
+        homeViewObjects = [HomeViewObject(id: latestTVShowResult.id, title: latestTVShowResult.originalName, posterLink: "https://image.tmdb.org/t/p/w200" + (latestTVShowResult.posterPath ?? ""))]
     }
 }
