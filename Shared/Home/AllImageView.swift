5f5928fe74282590c6c3fd00c8185bb47960df86
Tuyen Le <electro@Tuyens-iMac.local>
2022-01-25 19:39:04 -0500

    add AsyncImage to display images

--- Shared/Home/AllImageView.swift
+++ Shared/Home/AllImageView.swift
@@ -9,10 +9,30 @@ import SwiftUI
 
 struct AllImageView: View {
     let navigationTitle: String
+    let imagesLink: [String]
 
     var body: some View {
         ScrollView {
-            
+            LazyVStack(alignment: .center, spacing: 0) {
+                ForEach(imagesLink, id: \.self) { link in
+                    AsyncImage(url: URL(string: link), transaction: Transaction(animation: .linear)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                                .frame(width: UIScreen.main.bounds.width, alignment: .center)
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: UIScreen.main.bounds.width, alignment: .center)
+                        case .failure:
+                            EmptyView()
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                }
+            }
         }
         .navigationTitle(navigationTitle)
     }
@@ -20,6 +40,6 @@ struct AllImageView: View {
 
 struct ImageView_Previews: PreviewProvider {
     static var previews: some View {
-        AllImageView(navigationTitle: "")
+        AllImageView(navigationTitle: "", imagesLink: [])
     }
 }
